ab7e3c5824e32a5ac5c62ad4d96f13232f27174e
Jithinppp <jithinp968@gmail.com>
2022-06-16 17:02:43 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This is an app you can see latest musical events and events DJ parties
+      </p>
+      <p>Version 1.0.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,3 @@
+export default function AddPage() {
+  return <div>AddPage</div>;
+}
