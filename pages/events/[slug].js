e08f5f2a7cb3e8407c5598ee64808e69986aae2b
Jithinppp <jithinp968@gmail.com>
2022-06-19 01:46:51 +0530

    added single event page and styled

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,5 +1,9 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/event.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { FaPencilAlt, FaTrash } from "react-icons/fa";
 
 export default function EventPage({ event }) {
   return (
@@ -8,7 +12,35 @@ export default function EventPage({ event }) {
       metaDsc={"single Event"}
       keywords={"event,Dj,music Event"}
     >
-      {event.name}
+      <div className={styles.eventContainer}>
+        <div className={styles.controlsContainer}>
+          <Link href={`/events/edit/${event.id}`}>
+            <span className={styles.edit}>
+              <FaPencilAlt />
+              Edit
+            </span>
+          </Link>
+          <Link href={`/events/edit/${event.id}`}>
+            <span className={styles.delete}>
+              <FaTrash />
+              Delete
+            </span>
+          </Link>
+        </div>
+        <h2 className={styles.title}>{event.name}</h2>
+        <div className={styles.date}>{event.date}</div>
+        <div className={styles.imageContainer}>
+          <Image
+            width={960}
+            height={600}
+            alt="event-image"
+            src={event.image}
+            className={styles.image}
+          />
+        </div>
+        <p className={styles.performers}>{event.performers}</p>
+        <p className={styles.description}>{event.description}</p>
+      </div>
     </Layout>
   );
 }
@@ -27,7 +59,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/api/events/${slug}`);
   const data = await res.json();
-  console.log(API_URL);
+  console.log(data);
 
   return {
     props: {
